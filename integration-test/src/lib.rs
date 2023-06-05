296521a488bca98e8d80e445f376c93cdad7c4a6
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2023-06-05 23:15:27 +0800

    Add more tests

--- integration-test/src/lib.rs
+++ integration-test/src/lib.rs
@@ -2,11 +2,53 @@
 
 use documented::Documented;
 
-/// 69
-#[derive(Documented)]
-struct Nice;
-
 #[test]
 fn it_works() {
+    /// 69
+    #[derive(Documented)]
+    struct Nice;
+
     assert_eq!(Nice::DOCS, "69");
+}
+
+#[test]
+fn multi_line_works() {
+    /// 69
+    /// 420
+    ///
+    /// 1337
+    #[derive(Documented)]
+    struct Nice;
+
+    let docs = "69\n420\n\n1337";
+    assert_eq!(Nice::DOCS, docs);
+}
+
+#[test]
+fn every_style_works() {
+    /// 69
+    /** Very nice
+    420 */
+    #[doc = "1337"]
+    #[derive(Documented)]
+    struct Nicer;
+
+    let docs = "69\nVery nice\n420\n1337";
+    assert_eq!(Nicer::DOCS, docs);
+}
+
+#[test]
+fn readme_example_works() {
+    /// Nice.
+    ///
+    /// Multi-line doc comments are concatenated.
+    #[doc = "Attribute-style documentation is supported too."]
+    #[derive(documented::Documented)]
+    struct BornIn69;
+
+    let doc_str = "Nice.
+
+Multi-line doc comments are concatenated.
+Attribute-style documentation is supported too.";
+    assert_eq!(BornIn69::DOCS, doc_str);
 }
