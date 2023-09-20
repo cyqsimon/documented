baef5e541560e359a2a50a7ec62e13c2ee7072f4
Uriel <urielfontan2002@gmail.com>
2023-09-20 06:00:52 -0300

    Add nix support (#4)
    * add nix support
    
    * Fix description

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Trait and derive macro for accessing your type's documentation at runtime";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustTarget =
+          pkgs.rust-bin.stable.latest.default.override
+          {
+            extensions = ["rust-analyzer" "rust-src"];
+          };
+        nativeBuildInputs = with pkgs; [
+          curl
+          gcc
+          pkg-config
+          which
+          zlib
+        ];
+        buildInputs = with pkgs; [
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs =
+            nativeBuildInputs
+            ++ [
+            ];
+          buildInputs =
+            buildInputs
+            ++ [
+              rustTarget
+            ];
+        };
+      }
+    );
+}
