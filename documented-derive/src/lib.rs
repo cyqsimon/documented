5e94ac776a28ca0a04aa3915576cab386399d08b
ImUrX <urielfontan2002@gmail.com>
2023-09-20 04:45:42 -0300

    make it work

--- documented-derive/src/lib.rs
+++ documented-derive/src/lib.rs
@@ -1,59 +1,122 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, spanned::Spanned, DeriveInput, Error, Expr, ExprLit, Lit, Meta};
+use syn::{
+    parse_macro_input, parse_quote, spanned::Spanned, Attribute, Data, DataEnum, DataStruct,
+    DataUnion, DeriveInput, Error, Expr, ExprLit, Lit, Meta, Path,
+};
+
+fn crate_module_path() -> Path {
+    parse_quote!(::documented)
+}
 
 /// Derive proc-macro for `Documented` trait.
 #[proc_macro_derive(Documented)]
 pub fn documented(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let ident = input.ident;
+    let doc_comments = match get_comment(input.attrs.clone()) {
+        Ok(Some(r)) => r,
+        // Should we use an Option instead?
+        Ok(None) => "".to_string(),
+        Err(e) => return e.into_compile_error().into(),
+    };
 
-    let doc_comments = {
-        let maybe_str_literals = input
-            .attrs
-            .into_iter()
-            .filter_map(|attr| match attr.meta {
-                Meta::NameValue(name_value) if name_value.path.is_ident("doc") => {
-                    Some(name_value.value)
-                }
-                _ => None,
-            })
-            .map(|expr| match expr {
-                Expr::Lit(ExprLit { lit: Lit::Str(s), .. }) => Ok(s.value()),
-                e => Err(e),
-            })
-            .collect::<Result<Vec<_>, _>>();
-
-        let literals = match maybe_str_literals {
-            Ok(lits) => lits,
-            Err(expr) => {
-                return Error::new(expr.span(), "Doc comment is not a string literal")
-                    .into_compile_error()
-                    .into()
+    let fields_doc_comments = {
+        let fields_attrs: Vec<(syn::Ident, Vec<Attribute>)> = match input.data.clone() {
+            Data::Enum(DataEnum { variants, .. }) => {
+                variants.into_iter().map(|v| (v.ident, v.attrs)).collect()
             }
+            Data::Struct(DataStruct { fields, .. }) => fields
+                .into_iter()
+                .filter(|f| f.ident.is_some())
+                .map(|f| (f.ident.unwrap(), f.attrs))
+                .collect(),
+            Data::Union(DataUnion { fields, .. }) => fields
+                .named
+                .into_iter()
+                .map(|f| (f.ident.unwrap(), f.attrs))
+                .collect(),
         };
 
-        if literals.len() == 0 {
-            return Error::new(ident.span(), "No doc comment found on this type")
-                .into_compile_error()
-                .into();
+        match fields_attrs
+            .into_iter()
+            .map(|(i, attrs)| get_comment(attrs).map(|c| (i, c.unwrap_or_default())))
+            .collect::<syn::Result<Vec<_>>>()
+        {
+            Ok(t) => t,
+            Err(e) => return e.into_compile_error().into(),
         }
-
-        let trimmed: Vec<_> = literals
-            .iter()
-            .flat_map(|lit| lit.split("\n").collect::<Vec<_>>())
-            .map(|line| line.trim().to_string())
-            .collect();
-
-        trimmed.join("\n")
     };
+
+    let ident = input.ident;
+
+    let (field_idents, field_comments): (Vec<_>, Vec<_>) = fields_doc_comments.into_iter().unzip();
+
+    let phf_match_arms: Vec<_> = field_idents
+        .into_iter()
+        .map(|ident| ident.to_string())
+        .enumerate()
+        .map(|(i, ident)| quote! { #ident => #i, })
+        .collect();
+
+    let documented_module_path = crate_module_path();
 
     quote! {
         #[automatically_derived]
         impl documented::Documented for #ident {
             const DOCS: &'static str = #doc_comments;
+
+            const FIELD_DOCS: &'static [&'static str] = &[#(#field_comments),*];
+
+            fn get_index_by_name<T: AsRef<str>>(field_name: T) -> Option<usize> {
+                use #documented_module_path::_private_phf_reexport_for_macro as phf;
+
+                static PHF: phf::Map<&'static str, usize> = phf::phf_map! {
+                    #(#phf_match_arms)*
+                };
+                PHF.get(field_name.as_ref()).copied()
+            }
         }
     }
     .into()
+}
+
+fn get_comment(attrs: Vec<Attribute>) -> syn::Result<Option<String>> {
+    let maybe_str_literals = attrs
+        .into_iter()
+        .filter_map(|attr| match attr.meta {
+            Meta::NameValue(name_value) if name_value.path.is_ident("doc") => {
+                Some(name_value.value)
+            }
+            _ => None,
+        })
+        .map(|expr| match expr {
+            Expr::Lit(ExprLit {
+                lit: Lit::Str(s), ..
+            }) => Ok(s.value()),
+            e => Err(e),
+        })
+        .collect::<Result<Vec<_>, _>>();
+
+    let literals = match maybe_str_literals {
+        Ok(lits) => lits,
+        Err(expr) => {
+            return Err(Error::new(
+                expr.span(),
+                "Doc comment is not a string literal",
+            ))
+        }
+    };
+
+    if literals.is_empty() {
+        return Ok(None);
+    }
+
+    let trimmed: Vec<_> = literals
+        .iter()
+        .flat_map(|lit| lit.split("\n").collect::<Vec<_>>())
+        .map(|line| line.trim().to_string())
+        .collect();
+
+    Ok(Some(trimmed.join("\n")))
 }
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,8 +1,21 @@
 pub use documented_derive::Documented;
 
+#[doc(hidden)]
+pub use phf as _private_phf_reexport_for_macro;
+
 /// Adds an associated constant `DOCS` on your type containing its documentation,
 /// allowing you to access its documentation at runtime.
 pub trait Documented {
     /// The static doc comments on this type.
     const DOCS: &'static str;
+
+    /// Each index is a different field docs.
+    /// It's indexed by field order.
+    const FIELD_DOCS: &'static [&'static str];
+
+    fn get_index_by_name<T: AsRef<str>>(field_name: T) -> Option<usize>;
+
+    fn get_field_comment<T: AsRef<str>>(field_name: T) -> Option<&'static str> {
+        Self::get_index_by_name(field_name).map(|i| Self::FIELD_DOCS[i])
+    }
 }
