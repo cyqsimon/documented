2f7df2ad4964919b506f83fc06984037479e32d4
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2024-04-01 13:16:16 +0800

    Documentation rework

--- README.md
+++ README.md
@@ -2,79 +2,50 @@
 
 Derive macro for accessing your type's documentation at runtime
 
-[crates.io](https://crates.io/crates/documented)
+- [crates.io](https://crates.io/crates/documented)
+- [docs.rs](https://docs.rs/documented/latest/documented/)
 
 ## Quick start
 
 ```rust
-use documented::{Documented, DocumentedFields, Error};
+use documented::{Documented, DocumentedFields, DocumentedVariants, Error};
 
-/// Nice.
-/// Multiple single-line doc comments are supported.
-///
-/** Multi-line doc comments are supported too.
-    Each line of the multi-line block is individually trimmed.
-    Note the lack of spaces in front of this line.
-*/
-#[doc = "Attribute-style documentation is supported too."]
-#[derive(Documented, DocumentedFields)]
-struct BornIn69 {
-    /// Doc comments on fields (and enum variants) are supported too using
-    /// the `DocumentedFields` derive macro.
-    ///
-    /// Frankly, delicious.
-    rawr: String,
-
-    explosive: usize,
-};
-
-// `documented::Documented` usage:
-// ==================================================
-
-let doc_str = "Nice.
-Multiple single-line doc comments are supported.
-
-Multi-line doc comments are supported too.
-Each line of the multi-line block is individually trimmed.
-Note the lack of spaces in front of this line.
-
-Attribute-style documentation is supported too.";
-assert_eq!(BornIn69::DOCS, doc_str);
-
-// `documented::DocumentedFields` usage:
-// ==================================================
-
-let field_doc_str = "Doc comments on fields (and enum variants) are supported too using
-the `DocumentedFields` derive macro.
-
-Frankly, delicious.";
-assert_eq!(BornIn69::FIELD_DOCS, [Some(field_doc_str), None]);
-assert_eq!(BornIn69::get_field_docs("rawr"), Ok(field_doc_str));
-assert_eq!(
-    BornIn69::get_field_docs("explosive"),
-    Err(Error::NoDocComments("explosive".to_string()))
-);
-assert_eq!(
-    BornIn69::get_field_docs("gotcha"),
-    Err(Error::NoSuchField("gotcha".to_string()))
-);
-
-// `documented::DocumentedFields` usage:
-// ==================================================
-
-#[derive(DocumentedVariants)]
-enum NeverPlay {
-    F3,
-    /// I fell out of my chair.
+/// Trying is the first step to failure.
+#[derive(Documented, DocumentedFields, DocumentedVariants)]
+enum AlwaysPlay {
+    Kb1,
+    /// But only if you are white.
     F6,
 }
 
+// Documented
+assert_eq!(AlwaysPlay::DOCS, "Trying is the first step to failure.");
+
+// DocumentedFields
 assert_eq!(
-    NeverPlay::F3.get_variant_docs(),
-    Err(Error::NoDocComments("F3".into()))
+    AlwaysPlay::FIELD_DOCS,
+    [None, Some("But only if you are white.")]
 );
 assert_eq!(
-    NeverPlay::F6.get_variant_docs(),
-    Ok("I fell out of my chair.")
+    AlwaysPlay::get_field_docs("Kb1"),
+    Err(Error::NoDocComments("Kb1".to_string()))
+);
+assert_eq!(
+    AlwaysPlay::get_field_docs("F6"),
+    Ok("But only if you are white.")
+);
+assert_eq!(
+    AlwaysPlay::get_field_docs("Bf1"),
+    Err(Error::NoSuchField("Bf1".to_string()))
+);
+
+// DocumentedVariants
+assert_eq!(
+    AlwaysPlay::Kb1.get_variant_docs(),
+    Err(Error::NoDocComments("Kb1".to_string()))
+);
+assert_eq!(
+    AlwaysPlay::F6.get_variant_docs(),
+    Ok("But only if you are white.")
 );
 ```
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -3,8 +3,35 @@ pub use documented_derive::{Documented, DocumentedFields, DocumentedVariants};
 #[doc(hidden)]
 pub use phf as _private_phf_reexport_for_macro;
 
-/// Adds an associated constant `DOCS` on your type containing its documentation,
-/// allowing you to access its documentation at runtime.
+/// Adds an associated constant [`DOCS`](Self::DOCS) on your type containing its
+/// documentation, allowing you to access its documentation at runtime.
+///
+/// # Example
+///
+/// ```rust
+/// use documented::Documented;
+///
+/// /// Nice.
+/// /// Multiple single-line doc comments are supported.
+/// ///
+/// /** Multi-line doc comments are supported too.
+///     Each line of the multi-line block is individually trimmed.
+///     Note the lack of spaces in front of this line.
+/// */
+/// #[doc = "Attribute-style documentation is supported too."]
+/// #[derive(Documented)]
+/// struct BornIn69;
+///
+/// let doc_str = "Nice.
+/// Multiple single-line doc comments are supported.
+///
+/// Multi-line doc comments are supported too.
+/// Each line of the multi-line block is individually trimmed.
+/// Note the lack of spaces in front of this line.
+///
+/// Attribute-style documentation is supported too.";
+/// assert_eq!(BornIn69::DOCS, doc_str);
+/// ```
 pub trait Documented {
     /// The static doc comments on this type.
     const DOCS: &'static str;
@@ -14,13 +41,51 @@ pub trait Documented {
 /// documentation of its fields, allowing you to access their documentation at
 /// runtime.
 ///
-/// You can also use [`DocumentedFields::get_field_docs`] to access the
+/// This trait and associated derive macro works on structs, enums, and unions.
+///
+/// # Example
+///
+/// ```rust
+/// use documented::DocumentedFields;
+///
+/// #[derive(DocumentedFields)]
+/// struct BornIn69 {
+///     /// Frankly, delicious.
+///     rawr: String,
+///     explosive: usize,
+/// };
+///
+/// assert_eq!(BornIn69::FIELD_DOCS, [Some("Frankly, delicious."), None]);
+/// ```
+///
+/// You can also use [`get_field_docs`](Self::get_field_docs) to access the
 /// fields' documentation using their names.
+///
+/// ```rust
+/// # use documented::{DocumentedFields, Error};
+/// #
+/// # #[derive(DocumentedFields)]
+/// # struct BornIn69 {
+/// #     /// Frankly, delicious.
+/// #     rawr: String,
+/// #     explosive: usize,
+/// # };
+/// #
+/// assert_eq!(BornIn69::get_field_docs("rawr"), Ok("Frankly, delicious."));
+/// assert_eq!(
+///     BornIn69::get_field_docs("explosive"),
+///     Err(Error::NoDocComments("explosive".to_string()))
+/// );
+/// assert_eq!(
+///     BornIn69::get_field_docs("gotcha"),
+///     Err(Error::NoSuchField("gotcha".to_string()))
+/// );
+/// ```
 ///
 /// For enums, you may find [`DocumentedVariants`] more ergonomic to use.
 pub trait DocumentedFields {
-    /// The static doc comments on each field or variant of this type, indexed by
-    /// field/variant order.
+    /// The static doc comments on each field or variant of this type, indexed
+    /// by field/variant order.
     const FIELD_DOCS: &'static [Option<&'static str>];
 
     /// Method internally used by `documented`.
@@ -30,7 +95,8 @@ pub trait DocumentedFields {
     /// Get a field's documentation using its name.
     ///
     /// Note that for structs with anonymous fields (i.e. tuple structs), this
-    /// method will always return [`Error::NoSuchField`].
+    /// method will always return [`Error::NoSuchField`]. For this case, use
+    /// [`FIELD_DOCS`](Self::FIELD_DOCS) directly instead.
     fn get_field_docs<T: AsRef<str>>(field_name: T) -> Result<&'static str, Error> {
         let field_name = field_name.as_ref();
         let index = Self::__documented_get_index(field_name)
@@ -38,7 +104,7 @@ pub trait DocumentedFields {
         Self::FIELD_DOCS[index].ok_or_else(|| Error::NoDocComments(field_name.into()))
     }
 
-    /// Deprecated alias for [`DocumentedFields::get_field_docs`].
+    /// Deprecated alias for [`get_field_docs`](Self::get_field_docs).
     #[deprecated(
         since = "0.3.0",
         note = "This function has an inconsistent name. Use `DocumentedFields::get_field_docs` instead."
@@ -49,8 +115,33 @@ pub trait DocumentedFields {
     }
 }
 
-/// Adds an associated function [`DocumentedVariants::get_variant_docs`] to access
-/// the documentation on an enum variant.
+/// Adds an associated function [`get_variant_docs`](Self::get_variant_docs) to
+/// access the documentation on an enum variant.
+///
+/// This trait and associated derive macro works on enums only. For structs and
+/// unions, use [`DocumentedFields`] instead.
+///
+/// # Example
+///
+/// ```rust
+/// use documented::{DocumentedVariants, Error};
+///
+/// #[derive(DocumentedVariants)]
+/// enum NeverPlay {
+///     F3,
+///     /// I fell out of my chair.
+///     F6,
+/// }
+///
+/// assert_eq!(
+///     NeverPlay::F3.get_variant_docs(),
+///     Err(Error::NoDocComments("F3".into()))
+/// );
+/// assert_eq!(
+///     NeverPlay::F6.get_variant_docs(),
+///     Ok("I fell out of my chair.")
+/// );
+/// ```
 pub trait DocumentedVariants {
     /// Get the documentation on this enum variant.
     fn get_variant_docs(&self) -> Result<&'static str, Error>;
