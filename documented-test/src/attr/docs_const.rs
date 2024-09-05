3ca4aee71b6f66f76dac19586d3e4a03873c4627
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2024-09-05 17:24:42 +0800

    Update tests
    - Removed a few unnecesarily specific tests
    - Put customisation tests behind feature gate
    - Added a test for rename config option

--- documented-test/src/attr/docs_const.rs
+++ documented-test/src/attr/docs_const.rs
@@ -10,11 +10,11 @@ fn it_works() {
     assert_eq!(TEST_FN_DOCS, "This is a test function");
 }
 
-// Note: I found no way to test whether the visibility of the function is preserved.
+// Note: I found no way to test whether the visibility of the item is preserved.
 // Manual testing showed that it is preserved, but I couldn't find a way to test it in code.
 
 #[test]
-fn different_docs_work() {
+fn multiple_docs_work() {
     /// This is a test function
     /** This is the second line of the doc*/
     #[doc = "This is the third line of the doc"]
@@ -25,62 +25,42 @@ fn different_docs_work() {
     assert_eq!(TEST_FN_DOCS, "This is a test function\nThis is the second line of the doc\nThis is the third line of the doc");
 }
 
-#[test]
-fn parameters_work() {
-    /// This is a test function
-    #[docs_const]
-    #[allow(dead_code)]
-    fn test_fn(_test: String) {}
+#[cfg(feature = "customise")]
+mod test_customise {
+    use documented::docs_const;
 
-    assert_eq!(TEST_FN_DOCS, "This is a test function");
-}
+    #[test]
+    fn rename_works() {
+        /// Suspicious
+        #[docs_const(name = "NEVER_PLAY_F6")]
+        #[allow(dead_code)]
+        mod f6 {}
 
-#[test]
-fn return_and_body_works() {
-    /// This is a test function
-    #[docs_const]
-    #[allow(dead_code)]
-    fn test_fn(input: String) -> String {
-        input.to_ascii_lowercase()
+        assert_eq!(NEVER_PLAY_F6, "Suspicious");
     }
 
-    assert_eq!(TEST_FN_DOCS, "This is a test function");
-}
+    #[test]
+    fn trim_works() {
+        ///           This is a test function        
+        ///        Test Trim
+        #[docs_const(trim = true)] // technically redundant, as it's the default
+        #[allow(dead_code)]
+        fn test_fn() {}
 
-#[test]
-fn generics_and_lifetime_works() {
-    /// This is a test function
-    #[docs_const]
-    #[allow(dead_code)]
-    #[allow(clippy::extra_unused_lifetimes)]
-    fn test_fn<'a, T: std::fmt::Debug>(input: T) -> T {
-        input
+        assert_eq!(TEST_FN_DOCS, "This is a test function\nTest Trim");
     }
 
-    assert_eq!(TEST_FN_DOCS, "This is a test function");
-}
+    #[test]
+    fn no_trim_works() {
+        ///           This is a test function        
+        ///        Test Trim
+        #[docs_const(trim = false)]
+        #[allow(dead_code)]
+        fn test_fn() {}
 
-#[test]
-fn trim_works() {
-    ///           This is a test function        
-    ///        Test Trim
-    #[docs_const(trim = true)] // technically redundant, as it's the default
-    #[allow(dead_code)]
-    fn test_fn() {}
-
-    assert_eq!(TEST_FN_DOCS, "This is a test function\nTest Trim");
-}
-
-#[test]
-fn no_trim_works() {
-    ///           This is a test function        
-    ///        Test Trim
-    #[docs_const(trim = false)]
-    #[allow(dead_code)]
-    fn test_fn() {}
-
-    assert_eq!(
-        TEST_FN_DOCS,
-        "           This is a test function        \n        Test Trim"
-    ); // The whitespace is preserved, even on the end of the first line
+        assert_eq!(
+            TEST_FN_DOCS,
+            "           This is a test function        \n        Test Trim"
+        ); // The whitespace is preserved, even on the end of the first line
+    }
 }
